d1415f9eb043aa7fb457a48edf60a9144fd72981
Maviya Qureshi <111041905+MaviyaQureshi@users.noreply.github.com>
2023-01-19 21:54:27 +0530

    Add files via upload
    Summer Guideline page

---
+++ summer.js
@@ -0,0 +1,11 @@
+weather = document.getElementsById('weather')
+options = document.getElementsById('options')
+down = document.getElementsByClassName('.down')
+
+function myFunction() {
+    options.style.visibility = 'visible';
+}
+
+function myFunction2() {
+    options.style.visibility = 'hidden';
+}
